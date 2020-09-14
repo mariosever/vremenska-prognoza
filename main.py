c3f51dcb5f883b8f0d5be760db26fff18b72178b
Mario Sever <34382956+mariosever@users.noreply.github.com>
2020-09-14 20:10:54 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+
+    query = "Zagreb"
+    unit = "metric"
+    api_key = "a584720c43c130d020ae58d96440565e"
+
+    url = "https://api.openweathermap.org/data/2.5/weather?q={0}&units={1}&appid={2}&lang=HR".format(query, unit, api_key)
+
+    data = requests.get(url=url)
+
+    return render_template("index.html", data=data.json())
+
+
+if __name__ == '__main__':
+    app.run()
